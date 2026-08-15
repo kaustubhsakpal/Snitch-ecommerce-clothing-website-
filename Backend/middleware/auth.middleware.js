810a74d9d03a52ba7fab760691b85a970937b873
kaustubh <kaustubhsakpal9@gmail.com>
2026-08-15 21:15:39 +0530

    added middleware and register

--- Backend/middleware/auth.middleware.js
+++ Backend/middleware/auth.middleware.js
@@ -1,4 +1,7 @@
+import {user} from '../models/auth.model.js'
+
 export async function authmiddleware(req,res,next) {
-    
+
+   const token =req.cookies.login;
     
 }
--- Backend/routes/auth.route.js
+++ Backend/routes/auth.route.js
@@ -1,6 +1,7 @@
 import {Router} from 'express';
 import { authcontroller } from '../controllers/authcontroller..js';
+import { authmiddleware } from '../middleware/auth.middleware.js';
 
 export const Authrouter = Router();
 
-Authrouter.post("/register",authcontroller);
+Authrouter.post("/register",authmiddleware,authcontroller);
