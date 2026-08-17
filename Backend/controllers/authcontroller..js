bd844f4f42f4994c61aa17a096fb6fe140603baa
kaustubh <kaustubhsakpal9@gmail.com>
2026-08-17 23:45:15 +0530

    login

--- Backend/controllers/authcontroller..js
+++ Backend/controllers/authcontroller..js
@@ -1,30 +1,55 @@
-
 import { user } from "../models/auth.model.js";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+export const authcontroller = async (req, res) => {
+  const { username, email, password } = req.body;
 
-export const authcontroller= async (req,res)=>{    
-     const {username,email,password}=req.body;
+  const userallreadyexits = await user.findOne({
+    username,
+    email,
+    password,
+  });
+  if (userallreadyexits) {
+    return res.status(401).json({
+      message: "user allreday exits",
+    });
+  }
 
-     const userallreadyexits= await user.find({
-        username,email,password
-     })
-     if(userallreadyexits){
-        return res.status(401).json({
-            message:"user allreday exits"
-        })
-     }
+  const newuser = await user.create({
+    username,
+    email,
+    password,
+  });
 
-     const newuser = await user.create({
-        username,email,password
-     })
+  const registertoken =  jwt.sign({id:newuser._id}, process.env.JWT_SECRETS);
+  return res.status(200).json({
+    message: "user created scuessfully",
+    success: true,
+    newuser,
+    registertoken,
+  });
+};
 
-     const registertoken =jwt.sign("registertoken",process.env.JWT_SECRETS);
-     res.status(200).json({
-        message:"user created scuessfully",
-        success:true,
-        newuser,
-        registertoken
-     })
+export const logincontroller = async (req, res) => {
+  const { username, email, password } = req.body;
+  const userexits = await user.findOne({
+    $or: [{username}, {email}],
+  }).select("+password");
+  if (!userexits) {
+    return res.status(401).json({
+      message: "unathorized or crendital wrong",
+    });
+  }
+  console.log(userexits.password);
 
-}
-
+  const logintoken = jwt.sign({id:userexits._id}, process.env.JWT_SECRETS);
+  cook
+  const ismatch = await bcrypt.compare(password, (userexits.password));
+  if (ismatch) {
+    return res.status(200).json({
+      userexits,
+      message: "scessfully login",
+      logintoken,
+    });
+  }
+};
--- Backend/routes/auth.route.js
+++ Backend/routes/auth.route.js
@@ -1,7 +1,8 @@
 import {Router} from 'express';
-import { authcontroller } from '../controllers/authcontroller..js';
+import { authcontroller, logincontroller } from '../controllers/authcontroller..js';
 import { authmiddleware } from '../middleware/auth.middleware.js';
 
 export const Authrouter = Router();
 
 Authrouter.post("/register",authcontroller);
+Authrouter.post("/login",logincontroller);
