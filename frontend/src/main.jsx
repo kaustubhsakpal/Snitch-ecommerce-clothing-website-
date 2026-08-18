3da02d19baf9fd8a64b8962a02b410e7db8f9160
kaustubh <kaustubhsakpal9@gmail.com>
2026-08-18 18:39:47 +0530

    files and folder arranged

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,11 @@
 
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Login from './pages/auth/login/login.jsx';
+// import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    // <App />
+    <Login />
+    
 )
