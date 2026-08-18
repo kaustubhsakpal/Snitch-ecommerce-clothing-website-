8bc57cf23d0ac15bede3cb6f5da812f28dfd3f23
kaustubh <kaustubhsakpal9@gmail.com>
2026-08-19 00:01:38 +0530

    scss setup+login.jsx layout

--- frontend/src/pages/auth/login/Login.jsx
+++ frontend/src/pages/auth/login/Login.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
-
-const Login = () => {
+import './login.scss'
+const login = () => {
   return (
-    <div>Login</div>
+    <div className="maincontainer">
+        <div className="imagecontainer">
+            <div className="leftcontainer">
+                <div><h1>CompanyLogo</h1></div>
+                <div>
+                    <h1>Shop</h1>
+                 <div>
+                    <li>something</li>
+                    <li>something</li>
+                    <li>something</li>
+                    <li> something</li>
+                    <li>something</li>
+                 </div>
+
+                </div>
+            </div>
+            <div className="rightcontainer">
+                <div className="right">
+                    <div>X</div>
+                    <div>
+                        <h2>Existing Member</h2>
+                        <h2>Welcome Back</h2>
+                    </div>
+                    <div>
+                        <form action="">
+                            <input type="email" name="email" id="" />
+                            <input type="password" />
+                            <button>Continue</button>
+                        </form>
+                    </div>
+                    <div>------------OR-------------</div>
+                    <div>
+                        <img src="" alt="googlelogo" />
+                        <img src="" alt="lnkdinlogo" />
+                        <img src="" alt="githublogo" />
+                    </div>
+                    <div>
+                        <p> New here ? Create your account </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    
+    </div>
   )
 }
 
-export default Login
+export default login
